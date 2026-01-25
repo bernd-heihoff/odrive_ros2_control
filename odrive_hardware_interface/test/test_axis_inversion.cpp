82db8f326fb0a39100c38f1faa56d34f2424bc54
Bernd Heihoff <bernd.heihoff@web.de>
2026-01-25 17:27:53 +0000

    Remove unused axis and serial variables from AxisInversionTest for cleaner code

--- odrive_hardware_interface/test/test_axis_inversion.cpp
+++ odrive_hardware_interface/test/test_axis_inversion.cpp
@@ -141,8 +141,6 @@ TEST_F(AxisInversionTest, ConfigurationParsesInvertAxisParameter)
   TestHardwareInterface interface;
   interface.set_diagnostics_factory(make_fake_diagnostics_factory());
 
-  const int axis = 0;
-
   hardware_interface::HardwareInfo info;
   info.hardware_parameters["publish_diagnostics"] = "false";
 
@@ -168,8 +166,6 @@ TEST_F(AxisInversionTest, ConfigurationDefaultsInvertAxisToFalse)
 {
   TestHardwareInterface interface;
   interface.set_diagnostics_factory(make_fake_diagnostics_factory());
-
-  const std::int64_t serial = 0x00000000000000E1LL;
 
   hardware_interface::HardwareInfo info;
   info.hardware_parameters["publish_diagnostics"] = "false";
